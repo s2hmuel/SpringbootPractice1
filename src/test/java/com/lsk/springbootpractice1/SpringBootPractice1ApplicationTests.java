cfaca2fbfc0bc352a1782e24d3d7f042d105f751
s2hmuel <s2hmuel@gmail.com>
2024-01-19 15:15:01 +0900

    Init: 원격 리포지토리 연결

---
+++ src/test/java/com/lsk/springbootpractice1/SpringBootPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsk.springbootpractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
